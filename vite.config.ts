158eb818d90420294d5dc68b89e4aaa9545d96f1
arieffadhillah9-cpu <arieffadhillah9@gmail.com>
2026-07-03 05:42:16 +0700

    b

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/rise-foundation/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
